eea892e25a537ac009be79a5cb7bba0035e822df
Mathijs de Bruin <mathijs@mathijsfietst.nl>
2025-06-02 17:52:13 +0100

    Switch to Haiku and memory and conversations.

--- main.py
+++ main.py
@@ -6,9 +6,17 @@ from langchain.chat_models import init_chat_model
 from langchain.chat_models.base import BaseChatModel
 from langchain_core.messages import AnyMessage
 from langchain_core.runnables import RunnableConfig
+
 from langgraph.graph.graph import CompiledGraph
 from langgraph.prebuilt import create_react_agent
 from langgraph.prebuilt.chat_agent_executor import AgentState
+from langgraph.checkpoint.memory import InMemorySaver
+from langgraph.types import Checkpointer
+
+
+def get_checkpointer() -> Checkpointer:
+    checkpointer = InMemorySaver()
+    return checkpointer
 
 
 def get_weather(city: str) -> str:
@@ -17,7 +25,7 @@ def get_weather(city: str) -> str:
 
 
 def get_model() -> BaseChatModel:
-    model = init_chat_model("claude-sonnet-4-20250514", temperature=0.9)
+    model = init_chat_model("claude-3-5-haiku-latest", temperature=0.9)
     return model
 
 
@@ -29,11 +37,10 @@ def get_prompt(state: AgentState, config: RunnableConfig) -> list[AnyMessage]:
 
 def get_agent() -> CompiledGraph:
     model = get_model()
+    checkpointer = get_checkpointer()
 
     agent = create_react_agent(
-        model=model,
-        tools=[get_weather],
-        prompt=get_prompt,
+        model=model, tools=[get_weather], prompt=get_prompt, checkpointer=checkpointer
     )
 
     return agent
@@ -43,12 +50,19 @@ def main():
     load_dotenv()
 
     agent = get_agent()
-    result = agent.invoke(
-        {"messages": [{"role": "user", "content": "what is the weather in sf"}]},
-        config={"configurable": {"user_name": "John Smith"}},
+
+    config = RunnableConfig(configurable={"thread_id": "1"})
+
+    sf_response = agent.invoke(
+        {"messages": [{"role": "user", "content": "what is the weather in sf"}]}, config
     )
 
-    print(result)
+    print(sf_response["messages"][-1].content)
+
+    ny_response = agent.invoke(
+        {"messages": [{"role": "user", "content": "what about new york?"}]}, config
+    )
+    print(ny_response["messages"][-1].content)
 
 
 if __name__ == "__main__":
