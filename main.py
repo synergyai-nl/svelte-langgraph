39fee75aed3539454ae35275759b39330cd57c19
Mathijs de Bruin <mathijs@mathijsfietst.nl>
2025-06-02 17:37:56 +0100

    Hello world.

--- main.py
+++ main.py
@@ -1,5 +1,35 @@
+#!/usr/bin/env uv run python
+from dotenv import load_dotenv
+
+
+from langgraph.graph.graph import CompiledGraph
+from langgraph.prebuilt import create_react_agent
+
+
+def get_weather(city: str) -> str:
+    """Get weather for a given city."""
+    return f"It's always sunny in {city}!"
+
+
+def get_agent() -> CompiledGraph:
+    agent = create_react_agent(
+        model="claude-sonnet-4-20250514",
+        tools=[get_weather],
+        prompt="You are a helpful assistant",
+    )
+
+    return agent
+
+
 def main():
-    print("Hello from my-first-langgraph!")
+    load_dotenv()
+
+    agent = get_agent()
+    result = agent.invoke(
+        {"messages": [{"role": "user", "content": "what is the weather in sf"}]}
+    )
+
+    print(result)
 
 
 if __name__ == "__main__":
