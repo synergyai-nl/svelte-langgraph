ac17d9afbed38fb2a02856ed0fe46e64057ce34a
Mathijs de Bruin <mathijs@mathijsfietst.nl>
2025-06-03 16:45:09 +0100

    Styling and shiiiiiiiii

--- frontend/src/routes/+page.svelte
+++ frontend/src/routes/+page.svelte
@@ -2,33 +2,65 @@
 	import { SignIn, SignOut } from '@auth/sveltekit/components';
 	import { page } from '$app/stores';
 
-	import { Alert } from 'flowbite-svelte';
+	import { MessagesOutline, ArrowRightOutline } from 'flowbite-svelte-icons';
+	import {
+		Alert,
+		Button,
+		Navbar,
+		NavBrand,
+		NavLi,
+		NavUl,
+		NavHamburger,
+		DarkMode,
+		Heading
+	} from 'flowbite-svelte';
 </script>
 
-<h1>SvelteKit Auth Example</h1>
-<div class="p-8">
-	{#if $page.data.session}
-		<Alert>
-			<span class="font-medium">Info alert!</span>
-			<span class="signedInText">
-				<small>Signed in as</small><br />
-				<strong>{$page.data.session.user?.name ?? 'User'}</strong>
-			</span>
-		</Alert>
+<Navbar>
+	<NavBrand href="/">
+		<MessagesOutline class="me-3 h-6 sm:h-9" />
+		<span class="self-center text-xl font-semibold whitespace-nowrap dark:text-white"
+			>My AI Chat UI</span
+		>
+	</NavBrand>
+	<div class="flex items-center md:order-2">
+		<DarkMode />
+		<NavHamburger />
+	</div>
+	<NavUl>
+		<NavLi href="/">Home</NavLi>
+		<NavLi href="/about">About</NavLi>
+		<NavLi href="/docs/components/navbar">Navbar</NavLi>
+		<NavLi href="/pricing">Pricing</NavLi>
+		<NavLi href="/contact">Contact</NavLi>
+	</NavUl>
+</Navbar>
 
-		<SignOut>
-			<div slot="submitButton" class="buttonPrimary">Sign out</div>
-		</SignOut>
-	{:else}
-		<Alert>
-			<span class="font-medium">Info alert!</span>
-			<span class="notSignedInText">You are not signed in</span>
-		</Alert>
+<div class="text-center">
+	<Heading tag="h1" class="mb-4 text-4xl font-extrabold  md:text-5xl lg:text-6xl">
+		My AI Chat UI
+	</Heading>
 
-		<SignIn>
-			<div slot="submitButton" class="buttonPrimary">Sign in</div>
-		</SignIn>
-
-		<SignIn provider="descope" />
-	{/if}
+	<div class="p-8">
+		{#if $page.data.session}
+			<Alert>
+				<p class="mt-2 mb-4 text-sm">
+					<small>Signed in as</small><br />
+					<strong>{$page.data.session.user?.name ?? 'User'}</strong>
+				</p>
+				<SignOut>
+					<Button slot="submitButton">Sign out <ArrowRightOutline class="ms-2 h-6 w-6" /></Button>
+				</SignOut>
+			</Alert>
+		{:else}
+			<Alert color="green">
+				<p class="mt-2 mb-4 text-sm">You are not signed in</p>
+				<SignIn provider="descope">
+					<Button size="xs" color="green" slot="submitButton"
+						>Sign in <ArrowRightOutline class="ms-2 h-6 w-6" /></Button
+					>
+				</SignIn>
+			</Alert>
+		{/if}
+	</div>
 </div>
