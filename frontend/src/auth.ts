95e8c29b7c66355f69a9a5e4cb646309774e8747
Mathijs de Bruin <mathijs@mathijsfietst.nl>
2025-06-03 16:18:53 +0100

    Frontend auth!

---
+++ frontend/src/auth.ts
@@ -0,0 +1,6 @@
+import { SvelteKitAuth } from "@auth/sveltekit"
+import Descope from "@auth/sveltekit/providers/descope"
+
+export const { handle, signIn, signOut } = SvelteKitAuth({
+	providers: [Descope],
+})
--- frontend/src/routes/+page.svelte
+++ frontend/src/routes/+page.svelte
@@ -1,10 +1,34 @@
 <script>
+	import { SignIn, SignOut } from '@auth/sveltekit/components';
+	import { page } from '$app/stores';
+
 	import { Alert } from 'flowbite-svelte';
 </script>
 
+<h1>SvelteKit Auth Example</h1>
 <div class="p-8">
-	<Alert>
-		<span class="font-medium">Info alert!</span>
-		Change a few things up and try submitting again.
-	</Alert>
+	{#if $page.data.session}
+		<Alert>
+			<span class="font-medium">Info alert!</span>
+			<span class="signedInText">
+				<small>Signed in as</small><br />
+				<strong>{$page.data.session.user?.name ?? 'User'}</strong>
+			</span>
+		</Alert>
+
+		<SignOut>
+			<div slot="submitButton" class="buttonPrimary">Sign out</div>
+		</SignOut>
+	{:else}
+		<Alert>
+			<span class="font-medium">Info alert!</span>
+			<span class="notSignedInText">You are not signed in</span>
+		</Alert>
+
+		<SignIn>
+			<div slot="submitButton" class="buttonPrimary">Sign in</div>
+		</SignIn>
+
+		<SignIn provider="descope" />
+	{/if}
 </div>
