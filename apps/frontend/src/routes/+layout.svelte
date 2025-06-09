e0580da0794296623f03c36f388751b741b8eea2
Mathijs de Bruin <mathijs@mathijsfietst.nl>
2025-06-09 16:12:36 +0100

    Chat page and meaningful layout.

--- apps/frontend/src/routes/+layout.svelte
+++ apps/frontend/src/routes/+layout.svelte
@@ -1,7 +1,69 @@
 <script lang="ts">
 	import '../app.tailwind.css';
 
+	import { SignIn, SignOut } from '@auth/sveltekit/components';
+	import { page } from '$app/stores';
+
+	import { MessagesOutline, ArrowRightOutline } from 'flowbite-svelte-icons';
+	import {
+		Button,
+		Navbar,
+		NavBrand,
+		NavLi,
+		NavUl,
+		NavHamburger,
+		Dropdown,
+		DropdownHeader,
+		Avatar
+	} from 'flowbite-svelte';
+
 	let { children } = $props();
 </script>
+
+<Navbar>
+	<NavBrand href="/">
+		<MessagesOutline class="me-3 h-6 sm:h-9" />
+		<span class="self-center whitespace-nowrap text-xl font-semibold dark:text-white">
+			My AI Chat UI
+		</span>
+	</NavBrand>
+	<div class="flex items-center md:order-2">
+		{#if $page.data.session}
+			{#if $page.data.session.user?.image}
+				<Avatar id="avatar-menu" src={$page.data.session.user.image} />
+			{:else}
+				<Avatar id="avatar-menu" />
+			{/if}
+		{:else}
+			<SignIn provider="descope"><Button slot="submitButton" size="sm">Sign in</Button></SignIn>
+		{/if}
+		<NavHamburger />
+	</div>
+	{#if $page.data.session}
+		<Dropdown placement="bottom" triggeredBy="#avatar-menu">
+			<DropdownHeader>
+				<span class="block text-sm">{$page.data.session.user?.name ?? 'User'}</span>
+				<span class="block truncate text-sm font-medium">
+					{$page.data.session.user?.email ?? 'email'}
+				</span>
+			</DropdownHeader>
+			<!--
+			<DropdownGroup>
+				<DropdownItem>Dashboard</DropdownItem>
+				<DropdownItem>Settings</DropdownItem>
+				<DropdownItem>Earnings</DropdownItem>
+			</DropdownGroup>
+			-->
+			<SignOut>
+				<DropdownHeader>Sign out</DropdownHeader>
+			</SignOut>
+		</Dropdown>
+	{/if}
+	<NavUl>
+		<NavLi href="/">Home</NavLi>
+		<NavLi href="/chat">Chat</NavLi>
+		<NavLi href="/demo">Paraglide</NavLi>
+	</NavUl>
+</Navbar>
 
 {@render children()}
--- apps/frontend/src/routes/+page.svelte
+++ apps/frontend/src/routes/+page.svelte
@@ -2,69 +2,9 @@
 	import { SignIn, SignOut } from '@auth/sveltekit/components';
 	import { page } from '$app/stores';
 
-	import { MessagesOutline, ArrowRightOutline } from 'flowbite-svelte-icons';
-	import {
-		Alert,
-		Button,
-		Navbar,
-		NavBrand,
-		NavLi,
-		NavUl,
-		NavHamburger,
-		DarkMode,
-		Heading,
-		Dropdown,
-		DropdownHeader,
-		DropdownGroup,
-		DropdownItem,
-		Avatar
-	} from 'flowbite-svelte';
+	import { ArrowRightOutline } from 'flowbite-svelte-icons';
+	import { Alert, Button, Heading } from 'flowbite-svelte';
 </script>
-
-<Navbar>
-	<NavBrand href="/">
-		<MessagesOutline class="me-3 h-6 sm:h-9" />
-		<span class="self-center whitespace-nowrap text-xl font-semibold dark:text-white">
-			My AI Chat UI
-		</span>
-	</NavBrand>
-	<div class="flex items-center md:order-2">
-		{#if $page.data.session}
-			{#if $page.data.session.user?.image}
-				<Avatar id="avatar-menu" src={$page.data.session.user.image} />
-			{:else}
-				<Avatar id="avatar-menu" />
-			{/if}
-		{:else}
-			<SignIn provider="descope"><Button slot="submitButton" size="sm">Sign in</Button></SignIn>
-		{/if}
-		<NavHamburger />
-	</div>
-	{#if $page.data.session}
-		<Dropdown placement="bottom" triggeredBy="#avatar-menu">
-			<DropdownHeader>
-				<span class="block text-sm">{$page.data.session.user?.name ?? 'User'}</span>
-				<span class="block truncate text-sm font-medium">
-					{$page.data.session.user?.email ?? 'email'}
-				</span>
-			</DropdownHeader>
-			<!--
-			<DropdownGroup>
-				<DropdownItem>Dashboard</DropdownItem>
-				<DropdownItem>Settings</DropdownItem>
-				<DropdownItem>Earnings</DropdownItem>
-			</DropdownGroup>
-			-->
-			<SignOut>
-				<DropdownHeader>Sign out</DropdownHeader>
-			</SignOut>
-		</Dropdown>
-	{/if}
-	<NavUl>
-		<NavLi href="/">Home</NavLi>
-		<NavLi href="/demo">Demo</NavLi>
-	</NavUl>
-</Navbar>
 
 <div class="text-center">
 	<Heading tag="h1" class="mb-4 text-4xl font-extrabold  md:text-5xl lg:text-6xl">
