5e12cd8b28204b9c680fac48250b3a58ca0997f9
Mathijs de Bruin <mathijs@mathijsfietst.nl>
2025-06-28 19:54:37 +0100

    Transfer auth token to client.

--- apps/frontend/src/auth.ts
+++ apps/frontend/src/auth.ts
@@ -2,5 +2,20 @@ import { SvelteKitAuth } from '@auth/sveltekit';
 import Descope from '@auth/sveltekit/providers/descope';
 
 export const { handle, signIn, signOut } = SvelteKitAuth({
-	providers: [Descope]
+	providers: [Descope],
+	callbacks: {
+		session: async ({ session, token }) => {
+			if ('accessToken' in token) {
+				return { ...session, accessToken: token.accessToken };
+			}
+			return session;
+		},
+		jwt({ token, account }) {
+			if (account) {
+				// Add access token if provided.
+				return { ...token, accessToken: account.access_token };
+			}
+			return token;
+		}
+	}
 });
