b42f3892cecc72fb773e506d3a9ebb9300ffc966
pymeerkat <dhruv166arya@gmail.com>
2025-10-01 15:31:26 +0530

    On singout redirect to homepage (#54)

--- apps/frontend/src/routes/+layout.svelte
+++ apps/frontend/src/routes/+layout.svelte
@@ -72,7 +72,12 @@
 
 				<DropdownDivider />
 
-				<SignOut>
+				<SignOut
+					options={{
+						redirectTo: '/',
+						redirect: true
+					}}
+				>
 					<DropdownItem
 						slot="submitButton"
 						class="!flex !w-full !items-center !justify-start text-red-600 dark:text-red-500"
