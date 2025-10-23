da42f4ca1be5c6f2ab1fa5ed301f165a20c69d49
Mathijs de Bruin <mathijs@mathijsfietst.nl>
2025-10-23 11:12:11 +0100

    Factor out SignInButton, solve hydration error.

---
+++ apps/frontend/src/lib/auth/components/SignInButton.svelte
@@ -0,0 +1,9 @@
+<script>
+	import { SignIn } from '@auth/sveltekit/components';
+	import { Button } from 'flowbite-svelte';
+	import { m } from '$lib/paraglide/messages.js';
+</script>
+
+<SignIn provider="oidc">
+	<Button slot="submitButton" size="sm" tag="span">{m.auth_sign_in()}</Button>
+</SignIn>
