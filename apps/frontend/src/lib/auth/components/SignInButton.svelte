035392852f5711688d90e516bb8f264c6e6524e6
Mathijs de Bruin <mathijs@mathijsfietst.nl>
2025-11-10 15:14:07 +0000

    Solve hydration issue.

--- apps/frontend/src/lib/auth/components/SignInButton.svelte
+++ apps/frontend/src/lib/auth/components/SignInButton.svelte
@@ -1,9 +1,13 @@
 <script>
 	import { SignIn } from '@auth/sveltekit/components';
-	import { Button } from 'flowbite-svelte';
 	import { m } from '$lib/paraglide/messages.js';
 </script>
 
 <SignIn provider="oidc">
-	<Button slot="submitButton" size="sm" tag="span">{m.auth_sign_in()}</Button>
+	<span
+		slot="submitButton"
+		class="bg-primary-700 hover:bg-primary-800 dark:bg-primary-600 dark:hover:bg-primary-700 focus-within:ring-primary-300 dark:focus-within:ring-primary-800 inline-flex cursor-pointer items-center justify-center rounded-lg px-4 py-2 text-center text-sm font-medium text-white focus-within:ring-4 focus-within:outline-hidden"
+	>
+		{m.auth_sign_in()}
+	</span>
 </SignIn>
