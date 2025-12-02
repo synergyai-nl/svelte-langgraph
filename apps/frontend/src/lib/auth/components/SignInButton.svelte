183a0c367b1187b0eca7205f3303aae2137edf68
Dhruv <dhruv166arya@gmail.com>
2025-12-03 00:15:24 +0530

    Migrate SignInButton

--- apps/frontend/src/lib/auth/components/SignInButton.svelte
+++ apps/frontend/src/lib/auth/components/SignInButton.svelte
@@ -1,9 +1,9 @@
 <script>
 	import { SignIn } from '@auth/sveltekit/components';
-	import { Button } from 'flowbite-svelte';
+	import { Button } from '$lib/components/ui/button';
 	import { m } from '$lib/paraglide/messages.js';
 </script>
 
 <SignIn provider="oidc">
-	<Button slot="submitButton" size="sm" tag="span">{m.auth_sign_in()}</Button>
+	<Button variant="default" slot="submitButton" size="sm" tag="span">{m.auth_sign_in()}</Button>
 </SignIn>
