79cd8d1d264898865ba0f0dfa780697bbadd3edc
Mathijs de Bruin <git@github.com>
2026-01-30 16:56:32 +0000

    Various improvements to E2E tests.
    * Tweak page object model.
    * Ensure modal dialog sign in button redirects to login.
    * Ensure backend stuff is installed.
    * Explicit empty list for playwright install inputs.
    * Take screenshots on CI failures and upload reports.
    * Explicitly add $CI as input for E2E tests.
    * Let moon fully manage server processes:
    
    	Never re-use existing server and let moon handle
    	graceful shutdown (it will send SIGKILL after timeout
    	already and expects SIGINT).

--- e2e/src/pages/chat.page.ts
+++ e2e/src/pages/chat.page.ts
@@ -1,10 +1,11 @@
-import type { Locator, Page } from '@playwright/test';
+import type { Locator } from '@playwright/test';
+import { AppPage } from './app.page';
 
 /**
  * ChatPage encapsulates elements specific to the /chat route.
  */
 export class ChatPage {
-	readonly page: Page;
+	readonly app: AppPage;
 
 	// Chat elements
 	readonly textInput: Locator;
@@ -13,12 +14,12 @@ export class ChatPage {
 	readonly loginModal: Locator;
 	readonly modalSignInButton: Locator;
 
-	constructor(page: Page) {
-		this.page = page;
+	constructor(app: AppPage) {
+		this.app = app;
 
-		this.textInput = page.getByRole('textbox', { name: 'Message...' });
+		this.textInput = app.main.getByRole('textbox', { name: 'Message...' });
 
-		this.loginModal = page.getByRole('dialog').filter({ hasText: /sign in/i });
+		this.loginModal = app.page.getByRole('dialog').filter({ hasText: /sign in/i });
 		this.modalSignInButton = this.loginModal.getByText('Sign in', { exact: true });
 	}
 }
