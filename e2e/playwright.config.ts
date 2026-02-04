ca01f15c5e004b17ccee574b98d808b983c83dfa
Mathijs de Bruin <git@github.com>
2026-02-04 11:46:15 +0000

    Let moon fully manage server processes.
    Never re-use existing server and let moon handle
    graceful shutdown (it will send SIGKILL after timeout
    already and expects SIGINT).

--- e2e/playwright.config.ts
+++ e2e/playwright.config.ts
@@ -25,10 +25,9 @@ export default defineConfig({
 			command: 'moon backend:oidc-mock',
 			url: 'http://localhost:8080/.well-known/openid-configuration',
 			timeout: 120000,
-			reuseExistingServer: !process.env.CI,
 			stdout: 'ignore',
 			stderr: 'pipe',
-			gracefulShutdown: { signal: 'SIGTERM', timeout: 500 },
+			gracefulShutdown: { signal: 'SIGINT', timeout: 0 },
 			ignoreHTTPSErrors: false,
 			// @ts-expect-error: wait actually runs and exists but is not properly defined on the type.
 			wait: /Uvicorn running on http:\/\/localhost:8080/
@@ -38,9 +37,9 @@ export default defineConfig({
 			command: 'moon backend:serve-e2e',
 			url: 'http://localhost:2024/ok',
 			timeout: 120000,
-			reuseExistingServer: !process.env.CI,
 			stdout: 'ignore',
 			stderr: 'pipe',
+			gracefulShutdown: { signal: 'SIGINT', timeout: 0 },
 			// @ts-expect-error: wait actually runs and exists but is not properly defined on the type.
 			wait: /Registering graph with id/
 		},
@@ -49,10 +48,9 @@ export default defineConfig({
 			command: 'moon frontend:serve-e2e',
 			url: 'http://localhost:4173',
 			timeout: 120000,
-			reuseExistingServer: !process.env.CI,
 			stdout: 'ignore',
 			stderr: 'pipe',
-			gracefulShutdown: { signal: 'SIGTERM', timeout: 500 },
+			gracefulShutdown: { signal: 'SIGINT', timeout: 0 },
 			// @ts-expect-error: wait actually runs and exists but is not properly defined on the type.
 			wait: /http:\/\/localhost:4173/
 		}
