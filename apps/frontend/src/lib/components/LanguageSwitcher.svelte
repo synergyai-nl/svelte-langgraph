3b4040cced95254ffc5eb64552183a6ec223d25b
Mathijs de Bruin <mathijs@mathijsfietst.nl>
2026-02-11 15:15:11 +0000

    Fix button nesting in Tooltip components.
    Tooltip.Trigger renders as a button by default, causing invalid HTML
    when wrapping Button components. Refactored to use buttonVariants()
    helper directly on Tooltip.Trigger instead.

--- apps/frontend/src/lib/components/LanguageSwitcher.svelte
+++ apps/frontend/src/lib/components/LanguageSwitcher.svelte
@@ -1,7 +1,7 @@
 <script lang="ts">
 	import { getLocale, setLocale, locales, type Locale } from '$lib/paraglide/runtime.js';
 	import * as DropdownMenu from '$lib/components/ui/dropdown-menu';
-	import { Button } from '$lib/components/ui/button';
+	import { buttonVariants } from '$lib/components/ui/button';
 	import { Globe } from '@lucide/svelte';
 	import { m } from '$lib/paraglide/messages.js';
 	import { cn } from '$lib/utils.js';
@@ -19,30 +19,32 @@
 	}
 </script>
 
-<Tooltip.Root>
-	<Tooltip.Trigger>
-		<DropdownMenu.Root>
-			<DropdownMenu.Trigger>
-				{#snippet child({ props })}
-					<Button {...props} class={cn('', className)} variant="outline" size="sm">
-						<Globe size={16} />
-					</Button>
-				{/snippet}
-			</DropdownMenu.Trigger>
+<DropdownMenu.Root>
+	<DropdownMenu.Trigger>
+		{#snippet child({ props })}
+			<Tooltip.Root>
+				<Tooltip.Trigger
+					{...props}
+					class={cn(buttonVariants({ variant: 'outline', size: 'sm' }), className)}
+				>
+					<Globe size={16} />
+				</Tooltip.Trigger>
 
-			<DropdownMenu.Content align="end">
-				{#each locales as localeCode (localeCode)}
-					<DropdownMenu.Item onclick={() => switchLanguage(localeCode)}>
-						{m.local_name({}, { locale: localeCode })}
-						{#if getLocale() === localeCode}
-							<span class="ml-auto">✓</span>
-						{/if}
-					</DropdownMenu.Item>
-				{/each}
-			</DropdownMenu.Content>
-		</DropdownMenu.Root>
-	</Tooltip.Trigger>
-	<Tooltip.Content>
-		{m.local_name({}, { locale: getLocale() })}
-	</Tooltip.Content>
-</Tooltip.Root>
+				<Tooltip.Content>
+					{m.local_name({}, { locale: getLocale() })}
+				</Tooltip.Content>
+			</Tooltip.Root>
+		{/snippet}
+	</DropdownMenu.Trigger>
+
+	<DropdownMenu.Content align="end">
+		{#each locales as localeCode (localeCode)}
+			<DropdownMenu.Item onclick={() => switchLanguage(localeCode)}>
+				{m.local_name({}, { locale: localeCode })}
+				{#if getLocale() === localeCode}
+					<span class="ml-auto">✓</span>
+				{/if}
+			</DropdownMenu.Item>
+		{/each}
+	</DropdownMenu.Content>
+</DropdownMenu.Root>
