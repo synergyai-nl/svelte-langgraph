54e03ae95a57fc0b273f8246112c181dbb90b5e3
Dhruv <dhruv166arya@gmail.com>
2026-03-10 22:58:43 +0530

    Have Sentry Button only if DNS configured

--- apps/frontend/src/lib/components/SentryFeedbackButton.svelte
+++ apps/frontend/src/lib/components/SentryFeedbackButton.svelte
@@ -1,11 +1,13 @@
 <script lang="ts">
 	import * as Sentry from '@sentry/sveltekit';
+	import { env } from '$env/dynamic/public';
 	import { Button } from '$lib/components/ui/button';
 	import { MessageCircleHeart } from '@lucide/svelte';
 	import { cn } from '$lib/utils.js';
 	import * as Tooltip from '$lib/components/ui/tooltip/index.js';
 
 	let { class: className = '' } = $props();
+	const dsn = env.PUBLIC_SENTRY_DSN;
 
 	async function openFeedback() {
 		const feedback = Sentry.getFeedback();
@@ -17,19 +19,21 @@
 	}
 </script>
 
-<Tooltip.Root>
-	<Tooltip.Trigger>
-		{#snippet child({ props })}
-			<Button
-				{...props}
-				onclick={openFeedback}
-				class={cn('', className)}
-				variant="outline"
-				size="sm"
-			>
-				<MessageCircleHeart size={16} />
-			</Button>
-		{/snippet}
-	</Tooltip.Trigger>
-	<Tooltip.Content>Send Feedback</Tooltip.Content>
-</Tooltip.Root>
+{#if dsn}
+	<Tooltip.Root>
+		<Tooltip.Trigger>
+			{#snippet child({ props })}
+				<Button
+					{...props}
+					onclick={openFeedback}
+					class={cn('', className)}
+					variant="outline"
+					size="sm"
+				>
+					<MessageCircleHeart size={16} />
+				</Button>
+			{/snippet}
+		</Tooltip.Trigger>
+		<Tooltip.Content>Send Feedback</Tooltip.Content>
+	</Tooltip.Root>
+{/if}
